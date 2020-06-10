cc0dd34920798421974051673c4d4bd070907d2a
francesca-campana <franx3rox@gmail.com>
2020-06-10 21:28:43 +0200

    full

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
 $(document).ready(function () {
-  $('.with-dropdown').mouseenter(
-    function (){
+    $('.with-dropdown').mouseenter(
+      function (){
 
-      $(this).children('.dropdown').addClass('active');
+        $(this).children('.dropdown').addClass('active');
 
 
-  });
-  $('.with-dropdown').mouseleave(
-    function (){
-      $('.dropdown').removeClass('active');
-      $(this).children('with-dropdown').addClass('active');
+    });
+    $('.with-dropdown').mouseleave(
+      function (){
+        $('.dropdown').removeClass('active');
+        $(this).children('with-dropdown').addClass('active');
 
-  });
+    });
 
-  $('.with-dropdown').click(
-    function (){
-      $(this).children('.dropdown').toggleClass('active');
-    }
-  );
+    $('.with-dropdown').click(
+      function (){
+        $(this).children('.dropdown').toggleClass('active');
+      }
+    );
+
+    $('.btn').click(
+      function (){
+        $('.btn > .down').toggleClass('off');
+        $('.btn > .up').toggleClass('off')
+
+
+
+      }
+    );
+
+
 
   }
 );
